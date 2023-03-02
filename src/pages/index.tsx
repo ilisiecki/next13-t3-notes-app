46ed21011b1157f7f2be91bc6488517564825cda
ilisiecki <igor.fox.lisiecki@gmail.com>
2023-03-02 19:31:42 +0100

    Add create topic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,10 @@ import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Header from "~/components/Header";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
-import { api } from "~/utils/api";
+import { api, type RouterOutputs } from "~/utils/api";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -18,6 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <Header />
+        <Content />
       </main>
     </>
   );
@@ -25,26 +28,67 @@ const Home: NextPage = () => {
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
+type Topic = RouterOutputs["topic"]["getAll"][0];
+
+const Content: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
+
+  const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined }
+    {
+      enabled: sessionData?.user !== undefined,
+      onSuccess: (data) => {
+        setSelectedTopic(selectedTopic ?? data[0] ?? null);
+      },
+    }
   );
 
+  const createTopic = api.topic.create.useMutation({
+    onSuccess: () => {
+      toast.success("Topic created");
+      void refetchTopics();
+    },
+  });
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
+    <>
+      <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+        <div className="px-2">
+          <ul className="menu rounded-box w-56 bg-base-100 p-2">
+            {topics?.map((topic) => (
+              <li key={topic.id}>
+                <a
+                  href="#"
+                  onClick={(evt) => {
+                    evt.preventDefault();
+                    setSelectedTopic(topic);
+                  }}
+                >
+                  {topic.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="divider"></div>
+          <input
+            type="text"
+            placeholder="New Topic"
+            className="input-bordered input input-sm w-full"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                createTopic.mutate({
+                  title: e.currentTarget.value,
+                });
+                e.currentTarget.value = "";
+              }
+            }}
+          />
+          <div className="col-span-3"></div>
+        </div>
+      </div>
+      <Toaster position="bottom-right" reverseOrder={false} />
+    </>
   );
 };
