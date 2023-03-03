3d8271d49393a45eb57ce66b180dcc5e71cbee5a
ilisiecki <igor.fox.lisiecki@gmail.com>
2023-03-03 13:38:51 +0100

    Fix content view for not logged in users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,62 +74,72 @@ const Content: React.FC = () => {
 
   return (
     <>
-      <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
-        <div className="px-2">
-          <ul className="menu rounded-box w-56 bg-base-100 p-2">
-            {topics?.map((topic) => (
-              <li key={topic.id}>
-                <a
-                  href="#"
-                  onClick={(evt) => {
-                    evt.preventDefault();
-                    setSelectedTopic(topic);
-                  }}
-                >
-                  {topic.title}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <div className="divider"></div>
-          <input
-            type="text"
-            placeholder="New Topic"
-            className="input-bordered input input-sm w-full"
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                createTopic.mutate({
-                  title: e.currentTarget.value,
-                });
-                e.currentTarget.value = "";
-              }
-            }}
-          />
-        </div>
-        <div className="col-span-3">
-          <div>
-            {notes?.map((note) => (
-              <div key={note.id} className="mt-5">
-                <NoteCard
-                  note={note}
-                  onDelete={() => void deleteNote.mutate({ id: note.id })}
-                />
+      {sessionData?.user ? (
+        <>
+          <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+            <div className="px-2">
+              <ul className="menu rounded-box w-56 bg-base-100 p-2">
+                {topics?.map((topic) => (
+                  <li key={topic.id}>
+                    <a
+                      href="#"
+                      onClick={(evt) => {
+                        evt.preventDefault();
+                        setSelectedTopic(topic);
+                      }}
+                    >
+                      {topic.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <div className="divider"></div>
+              <input
+                type="text"
+                placeholder="New Topic"
+                className="input-bordered input input-sm w-full"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    createTopic.mutate({
+                      title: e.currentTarget.value,
+                    });
+                    e.currentTarget.value = "";
+                  }
+                }}
+              />
+            </div>
+            <div className="col-span-3">
+              <div>
+                {notes?.map((note) => (
+                  <div key={note.id} className="mt-5">
+                    <NoteCard
+                      note={note}
+                      onDelete={() => void deleteNote.mutate({ id: note.id })}
+                    />
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
 
-          <NoteEditor
-            onSave={({ title, content }) => {
-              void createNote.mutate({
-                title,
-                content,
-                topicId: selectedTopic?.id ?? "",
-              });
-            }}
-          />
-        </div>
-      </div>
-      <Toaster position="bottom-right" reverseOrder={false} />
+              <NoteEditor
+                onSave={({ title, content }) => {
+                  void createNote.mutate({
+                    title,
+                    content,
+                    topicId: selectedTopic?.id ?? "",
+                  });
+                }}
+              />
+            </div>
+          </div>
+          <Toaster position="bottom-right" reverseOrder={false} />
+        </>
+      ) : (
+        <>
+          <div className="flex items-center justify-center text-3xl text-white">
+            Please sign in see content
+          </div>
+        </>
+      )}
     </>
   );
 };
